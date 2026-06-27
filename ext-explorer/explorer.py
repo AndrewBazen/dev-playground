3e63fcbedc06ac9855d5a3857b1f91a87c6d6862
AndrewBazen <andrew.bazen@gmail.com>
2026-06-27 09:38:43 -0400

    Refactor extension counting logic to include file size and improve error handling
    - Updated the `collect` function to gather both the count and size of each file extension.
    - Changed variable names for clarity and updated the display function to reflect the new data structure.
    - Enhanced error handling for directory access issues.

--- ext-explorer/explorer.py
+++ ext-explorer/explorer.py
@@ -14,26 +14,32 @@ def main(args):
         print("Too many arguments")
         sys.exit()
 
-    ext_counts = collect(path)
+    # collect the info of all the different extensions in the Dir and exit if there is an error
+    ext_info = collect(path)
+    if ext_info is None:
+        sys.exit()
     
-    display(ext_counts)
+    display(ext_info)
 
 
 def collect(path):
     # new dictionary to hold {ext, # of times seen}
-    ext_counts = {}
+    ext_info = {}
 
+    # try to get the count and size of each file extension type.
     try:
-        for p in path.iterdir():
+        for p in path.rglob('*'):
             if p.is_dir():
                 continue
             ext = p.name
+            ext_size = p.stat().st_size
             if p.suffix != '':
                 ext = p.suffix
-            if ext in ext_counts:
-                ext_counts[ext] += 1
+            if ext in ext_info:
+                ext_info[ext]['count'] += 1
+                ext_info[ext]['size'] += ext_size
             else:
-                ext_counts[ext] = 1
+                ext_info[ext] = {'count': 1, 'size': ext_size}
     except FileNotFoundError:
         print("Directory not found")
         return None
@@ -41,7 +47,7 @@ def collect(path):
         print("Permission denied")
         return None
 
-    return ext_counts
+    return ext_info
 
 
 def display(info):
